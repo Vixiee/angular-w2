38a47ba2a8992d2ed71170a2acb068ec5c9301e1
Vixiee <134444300+Vixiee@users.noreply.github.com>
2024-05-01 17:25:35 +0300

    nqkvo

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BookRatingComponent } from './book-rating/book-rating.component';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, BookRatingComponent, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'angular-w2';
+}
