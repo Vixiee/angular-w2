a89b5aeaf42b5189549577fda69bffbbc58a0827
Vixiee <134444300+Vixiee@users.noreply.github.com>
2024-05-01 18:11:43 +0300

    ready

--- src/app/book-rating/book-rating.component.ts
+++ src/app/book-rating/book-rating.component.ts
@@ -21,47 +21,44 @@ interface Book {
 export class BookRatingComponent {
   books: Book[] = [
     {
-      title: 'To Kill a Mockingbird',
-      description:
-        'The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it.',
-      authors: ['Harper Lee'],
-      rating: 0,
-      ratings: [],
-      hasRated: false,
+      "title": "The Catcher in the Rye",
+      "description": "A novel narrated by the protagonist, Holden Caulfield, as he navigates his way through adolescence in New York City.",
+      "authors": ["J.D. Salinger"],
+      "rating": 0,
+      "ratings": [],
+      "hasRated": false
     },
     {
-      title: '1984',
-      description: 'A dystopian social science fiction novel by George Orwell.',
-      authors: ['George Orwell'],
-      rating: 0,
-      ratings: [],
-      hasRated: false,
+      "title": "Brave New World",
+      "description": "A dystopian novel set in a futuristic society where people are genetically engineered and conditioned for conformity and social stability.",
+      "authors": ["Aldous Huxley"],
+      "rating": 0,
+      "ratings": [],
+      "hasRated": false
     },
     {
-      title: 'The Great Gatsby',
-      description:
-        "F. Scott Fitzgerald's third book, stands as the supreme achievement of his career.",
-      authors: ['F. Scott Fitzgerald'],
-      rating: 0,
-      ratings: [],
-      hasRated: false,
+      "title": "Moby-Dick",
+      "description": "A novel about the voyage of the whaling ship Pequod, led by the monomaniacal Captain Ahab in pursuit of the giant white whale Moby Dick.",
+      "authors": ["Herman Melville"],
+      "rating": 0,
+      "ratings": [],
+      "hasRated": false
     },
     {
-      title: 'Pride and Prejudice',
-      description: 'An 1813 romantic novel of manners written by Jane Austen.',
-      authors: ['Jane Austen'],
-      rating: 0,
-      ratings: [],
-      hasRated: false,
+      "title": "The Lord of the Rings",
+      "description": "A high-fantasy epic that follows the quest to destroy the One Ring and defeat the Dark Lord Sauron, set in the fictional world of Middle-earth.",
+      "authors": ["J.R.R. Tolkien"],
+      "rating": 0,
+      "ratings": [],
+      "hasRated": false
     },
     {
-      title: "Harry Potter and the Philosopher's Stone",
-      description:
-        'The first novel in the Harry Potter series written by J.K. Rowling.',
-      authors: ['J.K. Rowling'],
-      rating: 0,
-      ratings: [],
-      hasRated: false,
+      "title": "The Adventures of Huckleberry Finn",
+      "description": "A novel narrated by the titular character, Huck Finn, as he embarks on a journey down the Mississippi River with a runaway slave named Jim, exploring themes of race, freedom, and morality.",
+      "authors": ["Mark Twain"],
+      "rating": 0,
+      "ratings": [],
+      "hasRated": false
     },
   ];
   currentBookIndex = 0;
@@ -75,7 +72,7 @@ export class BookRatingComponent {
   }
 
   finish() {
-    alert('Стига ти толкоз!');
+    alert('Готово е!');
   }
 
   get allBooksRated(): boolean {
